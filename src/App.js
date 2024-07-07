150f06353f0dd45d751df00ffc699dd7693aa01b
Manas Choudhary <mannchoudhary3431@gmail.com>
2024-07-07 17:24:38 +0530

    Creating States for description and quantity and also controlling elements

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -15,27 +16,43 @@ export default function App() {
 }
 
 function Logo() {
-  return <h1>🌴 Far Away 🧳</h1>;
+  return <h1>🌴 Far Away 🛍️</h1>;
 }
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
   function handleSubmit(e) {
     e.preventDefault();
+    if (!description) return;
+    const newItem = { id: Date.now(), quantity, description, packed: false };
+    console.log(newItem);
+    setDescription("");
+    setQuantity(1);
   }
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input text="item" placeholder="item.." />
+      <input
+        text="item"
+        placeholder="item.."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
 }
+
 function PackingList() {
   return (
     <div className="list">
